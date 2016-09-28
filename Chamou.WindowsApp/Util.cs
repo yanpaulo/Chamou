8656fa723d555daf100e83801d43970d3f15fadc
Yan Paulo <yanpaulo@hotmail.com>
2016-09-28 16:53:05 -0300

    Created class Util, with a single method (for now) RequestGeopositionAsync().

---
+++ Chamou.WindowsApp/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Chamou.WindowsApp
+{
+    public static class Util
+    {
+        public static async Task<BasicGeoposition> RequestGeopositionAsync()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus == GeolocationAccessStatus.Allowed)
+            {
+                // If DesiredAccuracy or DesiredAccuracyInMeters are not set (or value is 0), DesiredAccuracy.Default is used.
+                //Geolocator geolocator = new Geolocator { DesiredAccuracyInMeters = 1 };
+                Geolocator geolocator = new Geolocator();
+
+                // Carry out the operation
+                Geoposition pos = await geolocator.GetGeopositionAsync();
+                return pos.Coordinate.Point.Position;
+            }
+
+            throw new InvalidOperationException("Access to Geoposition not allowed");
+        }
+    }
+}
