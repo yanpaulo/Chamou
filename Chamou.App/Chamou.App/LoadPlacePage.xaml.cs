b5a5203be676bc2fd2b876bbb1c7a77573ba8262
Yan Paulo <yanpaulo@hotmail.com>
2016-09-30 14:45:48 -0300

    Implemented real location feature on Android. (NEXT: iOS)

--- Chamou.App/Chamou.App/LoadPlacePage.xaml.cs
+++ Chamou.App/Chamou.App/LoadPlacePage.xaml.cs
@@ -20,7 +20,7 @@ namespace Chamou.App
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            
+
             try
             {
                 var locator = CrossGeolocator.Current;
@@ -28,15 +28,21 @@ namespace Chamou.App
 
                 var position = await locator.GetPositionAsync();
                 var place = await WebService.GetPlaceByCoordinates(position.Latitude, position.Longitude);
-                await Navigation.PushAsync(new PlacePage() { BindingContext = place });
-                ;
+                if (place != null)
+                {
+                    await Navigation.PushAsync(new PlacePage() { BindingContext = place }); 
+                }
+                else
+                {
+                    throw new InvalidOperationException("Localização não cadastrada");
+                }
             }
             catch (Exception ex)
             {
-                await DisplayAlert("Erro", ex.Message, "Cancelar");
+                await DisplayAlert("Erro", ex.Message, "Ok");
                 //Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
             }
-            
+
         }
     }
 }
